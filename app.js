a141cb60927725c9397f235a766efcacd272e404
Joseph Martinez <joseph.r.martinez@gmail.com>
2023-05-20 12:00:33 -0700

    Update all read only routes

--- app.js
+++ app.js
@@ -15,7 +15,6 @@ const mongoose = require('mongoose');
 mongoose.set('strictQuery', false);
 const mongoDB = process.env.MONGODB_URI
 
-
 main().catch((err) => console.log(err));
 async function main() {
   await mongoose.connect(mongoDB);
